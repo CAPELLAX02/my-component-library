1243c331e0ce141bb20856c856e6886bf07db28e
CAPELLAX02 <ahmtatar@gmail.com>
2025-07-16 01:35:14 +0300

    Install and configure shadcn/ui successfuly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import {Button} from "@/components/ui/button.tsx";
 
 
 function App() {
   return (
-      <h1 className="text-amber-500 font-extrabold">FRESH REACT PROJECT STARTER</h1>
+      <div className="w-full h-full flex items-center justify-center min-h-screen">
+          <Button variant="default">Button</Button>
+          <Button variant="secondary">Button</Button>
+          <Button variant="link">Button</Button>
+          <Button variant="outline">Button</Button>
+          <Button variant="ghost">Button</Button>
+          <Button variant="destructive">Button</Button>
+      </div>
   )
 }
 
